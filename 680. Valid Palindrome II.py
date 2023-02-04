614b015933e416c570982e2d7d9d8feda9a4e120
Candost Akkaya <candost.akkaya@gmail.com>
2023-02-03 20:14:24 -0500

    Update 680. Valid Palindrome II.py

--- 680. Valid Palindrome II.py
+++ 680. Valid Palindrome II.py
@@ -9,6 +9,16 @@ class Solution:
                 s_end = s_rev[:i] + s_rev[i+1:]
                 print(s_front,s_end)
                 return s_front == s_front[::-1] or s_end == s_end[::-1]
+            
+            
+"""
+1) First we check if it's a palindrome
+2) Then we start checking if the opposite ends are the same, until we find a mismatch.
+3) When we find the mismatch we check if we can obtain a palindrome from either side, by removing the element we are at.
+4) if in either direction we find a palindrome, it returns True
+5) if neither is a palindrome, it means the string cannot be turned into a palindrome by moving at most one element, 
+and it returns false, since both of those equality statements are false  (return false or false)
+"""
       
                 
 
